b268197389e8da76d0e7bf4d67056602f507ddaa
Carlos <carlos_ds730@yahoo.es>
2015-12-24 09:19:13 -0500

    first commit

---
+++ TOWPEb_Main/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+
+# Create your views here.
+
+
+def home(request):
+    if request.method == 'GET':
+        return render(request, 'index.html', {})
+    elif request.method == 'POST':
+        try:
+            email = request.POST['inputEmail']
+            name = request.POST['inputSubject']
+            message = request.POST['message']
+            send_mail('An email from ' + name, message, email,
+                      ['carlosalejandro@towpeb.com'], fail_silently=False)
+            return render(request, 'index.html', {'OK': True})
+        except KeyError:
+            return render(request, 'index.html', {'Fail': True})
